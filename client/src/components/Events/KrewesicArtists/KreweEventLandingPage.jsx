4596a62697b79251878dd322bd848c60cc390ac4
Jacob Young <jyoung3128@gmail.com>
2021-10-24 15:29:44 -0500

    merging with alex's PR

--- client/src/components/Events/KrewesicArtists/KreweEventLandingPage.jsx
+++ client/src/components/Events/KrewesicArtists/KreweEventLandingPage.jsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 
 const StyledLanding = styled.div`
   .landingButton {
-    background-color: #b3a970; 
+    background-color: #b3a970;
   }
   .commentInput {
     width: 600px;
@@ -45,18 +45,18 @@ const KreweEventLandingPage = () => {
   };
 
   const postInterest = async () => {
-    await axios.post('/krewesicevents/interestedUser', {eventId})
-  }
+    await axios.post('/krewesicevents/interestedUser', {eventId});
+  };
 
   const getInterestedUsers = async () => {
-    const {data} = await axios.get(`/krewesicevents/interestedUsers/${eventId}`)
-    console.log(data)
-    setInterestedUsers(data)
-  }
+    const {data} = await axios.get(`/krewesicevents/interestedUsers/${eventId}`);
+    console.log(data);
+    setInterestedUsers(data);
+  };
 
 
   const getCommentWall = async() => {
-    
+
     const {data} = await axios.get(`/krewesicevents/commentWall/${eventId}`);
     setCommentWall(data);
   };
@@ -71,46 +71,46 @@ const KreweEventLandingPage = () => {
   useEffect(() => {
     getEventDeetz();
     getCommentWall();
-    getInterestedUsers()
+    getInterestedUsers();
   }, []);
 
 
   return (
     <StyledLanding>
-    <div>
-   
-      <h1>{artist}</h1>
-      <div>{dateTime}</div>
-      <div>{address}</div>
-      <div>{city}, {state}</div>
-      <div>{venue}</div>
+      <div>
 
-      <div>
-        <Button className='landingButton' onClick={postInterest}>interested</Button>
-      </div>
-      <div>
-        <h3>interested users</h3>
-        {interestedUsers.map((user, i) => <div>{user.User.name}</div>)}
-      </div>
-
-      <div>
-        <h3>comment wall</h3>
-        <TextField
-          placeholder='insightful and witty commentary'
-          className='inputBackground commentInput'
-          variant="outlined"
-          onChange={(e) => setCommentText(e.target.value)}
-          value={commentText}
-        />
-        <Button className='landingButton' onClick={postComment}>send comment</Button>
+        <h1>{artist}</h1>
+        <div>{dateTime}</div>
+        <div>{address}</div>
+        <div>{city}, {state}</div>
+        <div>{venue}</div>
 
         <div>
-          {commentWall.map((comment, i) => <CommentComponent key={i} comment={comment} />)}
+          <Button className='landingButton' onClick={postInterest}>interested</Button>
+        </div>
+        <div>
+          <h3>interested users</h3>
+          {interestedUsers.map((user, i) => <div>{user.User.name}</div>)}
+        </div>
+
+        <div>
+          <h3>comment wall</h3>
+          <TextField
+            placeholder='insightful and witty commentary'
+            className='inputBackground commentInput'
+            variant="outlined"
+            onChange={(e) => setCommentText(e.target.value)}
+            value={commentText}
+          />
+          <Button className='landingButton' onClick={postComment}>send comment</Button>
+
+          <div>
+            {commentWall.map((comment, i) => <CommentComponent key={i} comment={comment} />)}
+          </div>
+
         </div>
 
       </div>
-
-    </div>
     </StyledLanding>
   );
 };
