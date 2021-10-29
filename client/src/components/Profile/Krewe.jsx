95ee366773b2c801ef1a6660ebe75a08cb2fd52c
Jacob Young <jyoung3128@gmail.com>
2021-10-29 12:04:01 -0500

    Set description for random album received from api call on profile

--- client/src/components/Profile/Krewe.jsx
+++ client/src/components/Profile/Krewe.jsx
@@ -5,19 +5,34 @@ import axios from 'axios';
 
 const Krewe = ({ artist }) => {
   const [ artistAlbum, setArtistAlbum] = useState('');
+  const [ albumDesc, setAlbumDesc ] = useState('');
+  const randomAlbum = () => {
+    if (artistAlbum) {
+      setAlbumDesc('Random Album:');
+    } else {
+      setAlbumDesc(null);
+    }
+  };
 
-
+  useEffect(() => {
+    randomAlbum();
+  });
 
   return (
-    <div
-      onClick={() => axios.get(`https://theaudiodb.com/api/v1/json/523532/searchalbum.php?s=${artist}`).then((data) => {
+    <div>
+      <div
+        onClick={() => axios.get(`https://theaudiodb.com/api/v1/json/523532/searchalbum.php?s=${artist}`).then((data) => {
         //console.info(data.data.album);
-        const randomValue = data.data.album[Math.floor(Math.random() * data.data.album.length)];
-        console.info(randomValue.strAlbum);
-        setArtistAlbum(randomValue.strAlbum);
-      })}
-    >
-      {artist} {artistAlbum}
+          const randomValue = data.data.album[Math.floor(Math.random() * data.data.album.length)];
+          console.info(randomValue.strAlbum);
+          setArtistAlbum(randomValue.strAlbum);
+        })}
+      >
+        {artist}
+      </div>
+      <div>
+        {albumDesc}  {artistAlbum}
+      </div>
     </div>
   );
 };
